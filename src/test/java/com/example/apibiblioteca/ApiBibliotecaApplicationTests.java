47811e7ca81d910a504fe0f649072dafe17289b5
mariiana-lp <mariana.lopez.docs_221@gmail.com>
2022-04-05 11:17:54 -0500

    Inicio

---
+++ src/test/java/com/example/apibiblioteca/ApiBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apibiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
